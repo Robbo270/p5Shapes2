1afb025f4aefecb5890886835b3f04346eaa3b0d
Robbo270 <116882868+Robbo270@users.noreply.github.com>
2022-11-07 21:06:22 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -4,8 +4,53 @@ function setup() {
 }
 
 function draw() {
-    background(220);
+    background(0);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    fill(255, 0, 0);
+    ellipse(100, 100, 200);
+
+    fill(255, 107, 0);
+    ellipse(300, 100, 200);
+
+    fill(255, 214, 0);
+    ellipse(500, 100, 200);
+
+    fill(158, 255, 0);
+    ellipse(700, 100, 200);
+
+    fill(0, 255, 102);
+    ellipse(100, 300, 200);
+
+    fill(0, 255, 209);
+    ellipse(300, 300, 200);
+    
+    fill(0, 163, 255);
+    ellipse(500, 300, 200);
+
+    fill(0, 56, 255);
+    ellipse(700, 300, 200);
+
+    fill(158, 0, 255);
+    ellipse(100, 500, 200);
+    
+    fill(219, 0, 255);
+    ellipse(300, 500, 200);
+
+    fill(255, 0, 77);
+    ellipse(500, 500, 200);
+
+    fill(255, 0, 0);
+    ellipse(700, 500, 200);
+
+    fill(255, 138, 0);
+    ellipse(100, 700, 200);
+
+    fill(255, 214, 0);
+    ellipse(300, 700, 200);
+
+    fill(219, 0, 255);
+    ellipse(500, 700, 200);
+
+    fill(0, 255, 209);
+    ellipse(700, 700, 200);
 }
